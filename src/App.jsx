0001348820edf141acc78a9ad374bded090dbee1
sanjaykumar06-10 <thallapallisanjaykumar06@gmail.com>
2025-05-25 17:46:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ function App() {
          <Route path="/app5" element={<App5 />}></Route>
          <Route path="/app6" element={<App6 />}></Route>
          <Route path="/app7" element={<App7 />}></Route>
+         <Route path="/app8" element={<App8 />}></Route>
+         <Route path="/app9" element={<App9 />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
